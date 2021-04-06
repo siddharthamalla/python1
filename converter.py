54eae850a61ff95791ac1d6eff1f59748f5fbac8
siddhartha malla <ankitmalla2222@gmail.com>
2021-04-06 08:50:43 +0545

    init

---
+++ converter.py
@@ -0,0 +1,10 @@
+#Create a currency converter
+#fist download the forex-python
+from forex_python.converter import CurrencyRates
+c = CurrencyRates()
+amount = int(input("Enter the amount: "))
+from_currency = input("From Currency: ").upper()
+to_currency = input("To Currency: ").upper()
+print(from_currency, " To ", to_currency, amount)
+result = c.convert(from_currency, to_currency, amount)
+print(result)
